317db47f9408862dad376c8624588a99a2efa8e6
Omar <oalblaihy@gmail.com>
2023-02-20 14:07:00 +0300

    prod

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://http://udagram-api-dev.eba-xfmc8x3m.us-east-1.elasticbeanstalk.com:8080/api/v0'
 };
 
 /*
