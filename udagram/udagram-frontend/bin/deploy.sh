f92924c433b1f82c8486ed6b70e1c41a7c0c28fc
Omar <oalblaihy@gmail.com>
2023-02-20 05:30:26 +0300

    Update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,6 +1,6 @@
-aws s3 cp --recursive --acl public-read ./www s3://roone-udagram/ --profile default
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://roone-udagram/ --profile default
+aws s3 cp --recursive --acl public-read ./www s3://omar-123-udagram/ --profile default
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://omar-123-udagram/ --profile default
 
-# aws s3api create-bucket --bucket roone-udagram --region us-east-1 --profile default
-# aws s3 cp --recursive  ./www s3://roone-udagram/ --profile default   
-# aws s3 rm s3://roone-udagram --recursive --profile default
+# aws s3api create-bucket --bucket omar-123-udagram --region us-east-1 --profile default
+# aws s3 cp --recursive  ./www s3://omar-123-udagram/ --profile default   
+# aws s3 rm s3://omar-123-udagram --recursive --profile default
